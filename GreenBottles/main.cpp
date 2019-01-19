74ffcba6de68e2b83dcbd335d1b3fafcded18d6f
NickyDz <nickydcruz@yahoo.com>
2019-01-19 21:24:13 +0530

    Assignment1

---
+++ GreenBottles/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int greenBottles = 10;
+
+    cout << "There were " << greenBottles << " Green Bottles"<< endl;
+    greenBottles--;
+
+    cout << "Now there are " << greenBottles << " Green Bottles"<< endl;
+    return 0;
+}
